6f8dfb49f443e565fc8ef9d9d178f8c3c00ca0b9
Andreas <andreastaru007@gmail.com>
2024-07-05 21:18:45 +0300

    fixed font rendering bug

--- src/components/Loadingscreen/Loadingscreen.jsx
+++ src/components/Loadingscreen/Loadingscreen.jsx
@@ -1,7 +1,19 @@
 import styles from "./Loadingscreen.module.css";
 import { PuffLoader } from "react-spinners";
+import FontFaceObserver from "fontfaceobserver";
 
 const Loadingscreen = () => {
+  const robotoFont = new FontFaceObserver("Roboto");
+
+  robotoFont
+    .load()
+    .then(() => {
+      document.documentElement.style.fontFamily = "Roboto, sans-serif";
+    })
+    .catch(() => {
+      document.documentElement.style.fontFamily = "Arial, sans-serif";
+    });
+
   return (
     <div className={styles.loader}>
       <PuffLoader color={"#fff"} size={150} />
